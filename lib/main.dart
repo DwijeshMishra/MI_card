48b3c42a90dbf537a40a4897361944097cc26208
dwijeshmishra <dwijeshvasu515@gmail.com>
2020-07-29 16:01:59 +0530

    This is my New and first app

--- lib/main.dart
+++ lib/main.dart
@@ -9,74 +9,93 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'MyProfile',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontFamily: 'Source Sans Pro',
+            ),
+          ),
+        ),
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/angela.jpg'),
-            ),
-            Text(
-              'Angela Yu',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40.0,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('Image/pic.jpg'),
+              ),
+              Text(
+                'Dwijesh Mishra',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.black45,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 270.0,
+                child: Divider(
+                  color: Colors.blueGrey.shade100,
+                ),
+              ),
+              Text(
+                'Flutter Developer ',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 30.0,
+                    color: Colors.black45,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 220.0,
+                child: Divider(
+                  color: Colors.blueGrey.shade100,
+                ),
+              ),
+              Card(
                 color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                color: Colors.teal.shade100,
-                fontSize: 20.0,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-              width: 150.0,
-              child: Divider(
-                color: Colors.teal.shade100,
-              ),
-            ),
-            Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: ListTile(
                   leading: Icon(
-                    Icons.phone,
+                    Icons.add_call,
+                    size: 30.0,
                     color: Colors.teal,
                   ),
                   title: Text(
-                    '+44 123 456 789',
+                    '+917000036189',
                     style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
                       fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro',
                     ),
                   ),
-                )),
-            Card(
+                ),
+              ),
+              Card(
+                color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: ListTile(
                   leading: Icon(
-                    Icons.email,
+                    Icons.mail_outline,
+                    size: 30.0,
                     color: Colors.teal,
                   ),
                   title: Text(
-                    'angela@email.com',
+                    'Dwijesh515@gmail.com',
                     style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro'),
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro',
+                    ),
                   ),
-                ))
-          ],
-        )),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
